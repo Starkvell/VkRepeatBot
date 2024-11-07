5c2c3cb398d153a0f29278fb98e5d2167c4c772c
Ануфриев Павел Васильевич <p.anufriev@nicetu.spb.ru>
2024-11-07 17:34:34 +0300

    Initial commit

---
+++ src/main/java/com/example/vkrepeatbot/VkRepeatBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.vkrepeatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkRepeatBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkRepeatBotApplication.class, args);
+    }
+
+}
